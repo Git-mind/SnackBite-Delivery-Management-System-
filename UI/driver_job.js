c7ac53e26aa1ce805272d95c592cfafd8066f381
deathreaper98 <72450579+deathreaper98@users.noreply.github.com>
2021-03-30 22:54:18 +0800

    show jobs

--- UI/driver_job.js
+++ UI/driver_job.js
@@ -3,18 +3,43 @@ var delivery_management_URL = "http://localhost:5200/";
 var app = new Vue({
     el: "#app",
     computed: {
-        hasOrders: function () {
-            return this.orders.length > 0;
-        }
+
     },
     data: {
         "customers": [],
         message: "There is a problem retrieving books data, please try again later.",
         customer_id: "1",
         customer_name: "John",
+        no_customer:"",
     },
     methods: {
+        find_by_driver_id: function () {
+            // on Vue instance created, load the book list
+            const response =
+                // fetch(order_URL)
+                fetch(delivery_management_URL + "display_order")
+                .then(response => response.json())
+                .then(data => {
+                    console.log(response);
+                    if (data.code === 404) {
+                        // no order in db
+                        this.message = data.message;
+                        this.no_customer = false;
+                    } else {
+                        console.log(data)
+                        this.no_customer = true;
+                        this.customers = data.data.order_result.data.customers;
+                        this.message = "You have " + this.customers.length + " food orders"
+                    }
+                })
+                .catch(error => {
+                    // Errors when calling the service; such as network error, 
+                    // service offline, etc
+                    console.log(this.message + error);
 
+                });
+
+        },
         
     },
     created: function () {
@@ -22,6 +47,6 @@ var app = new Vue({
             userName = document.getElementById('userName')
             userName.innerHTML = 'Welcome back ' + this.customer_name + "!"
         }
-        this.drivercustomer_id();
+     
     }
 });
